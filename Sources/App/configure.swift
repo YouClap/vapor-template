b181b0c4e7c7d8a1dbe43489e280724cfa0c2e8f
portellaa <lcportellaa@gmail.com>
2019-07-01 18:48:21 +0100

    Adapt with Vapor 4 ideas and create DatabaseConnnectable

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,14 +1,20 @@
 import Vapor
 
 /// Called before your application initializes.
-public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    // Register routes to the router
-    let router = EngineRouter.default()
-    try routes(router)
-    services.register(router, as: Router.self)
+public func configure(_ env: inout Environment) throws -> (Config, Services) {
+    let config = Config.default()
+    var services = Services.default()
 
     // Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
+
+    services.register(Router.self) { container -> EngineRouter in
+        let router = EngineRouter.default()
+        try routes(router, container)
+        return router
+    }
+
+    return (config, services)
 }
